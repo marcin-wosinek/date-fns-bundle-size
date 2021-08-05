7d48b9c9ffb4c6da3de15c0e5d5c1dfef4629f6c
Marcin Wosinek <marcin@wosinek.eu>
2021-08-05 08:01:28 +0200

    add more method calls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { sub } from "date-fns";
+import { sub, startOfQuarter, format, differenceInDays } from "date-fns";
 
-const today = new Date();
+const today = new Date(),
+  quarterStart = startOfQuarter(today),
+  diffDays = differenceInDays(today, quarterStart);
 
 console.log("Yesterday was", sub(today, { days: 1 }));
+
+console.log(
+  "The quarter started on",
+  format(monthStart, "yyyy-mm-dd"),
+  "and today, it is",
+  diffDays,
+  "days since then"
+);
